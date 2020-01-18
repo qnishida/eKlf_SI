7036e6dc5ac71b44cf74d6863f3bd0f093110698
knishida <knishida@eri.u-tokyo.ac.jp>
2020-01-18 13:13:20 +0900

    First commit

---
+++ KalmanFilter.py
@@ -0,0 +1,278 @@
+import numpy as np
+import math 
+from scipy import signal
+from scipy import interpolate
+from scipy import fftpack
+import datetime
+
+
+class Klf:  
+    def init():#Initialize precipitation data
+        with open("./gwl_ebino_seikei.dat","r") as f: lines = f.readlines()
+        Klf.precipitation = np.array([float(line.split()[4]) for line in lines])
+        for i in range(len(lines)):
+            if int(lines[i].split()[5]) == 1: 
+                Klf.num0 = i
+        Klf.precipitation -= Klf.precipitation.mean()
+        #Klf.q_ref = np.array([1E-6,1E-9,100.,6.726449064255413e-07,-5.,1E-5,1E-8,1E-3])
+        Klf.q_ref = np.array([1E-6,1E-9,100.,6.726449064255413e-07,-5.,1E-3,-1E-3,30.])
+    def cal_kumamoto(t_kmt, at, τ, mask_param):
+        dv = at[:,1].copy() 
+        t0 = len(dv[:])
+        tx = np.arange(0,t0,1)
+        if len((dv[t_kmt-100:t_kmt-1])[mask_param[t_kmt-100:t_kmt-1]]) > 0:
+            y_ref = (dv[t_kmt-100:t_kmt-1])[mask_param[t_kmt-100:t_kmt-1]].mean()
+        else:
+            y_ref = 0
+        ye = np.zeros(t0) + y_ref
+
+        cross = 0.
+        ms = 0.
+        for i in range(t_kmt,t0):
+            η = np.exp(-(tx[i]-t_kmt)/τ)
+            cross += (η*(dv[i]-y_ref))
+            ms += (η*η)
+        A = cross/ms
+        #print(A,y_ref)
+        ye[t_kmt:] = A*np.exp(-(tx[t_kmt:]-t_kmt)/τ) + y_ref
+        res = ((ye[t_kmt:t0]-dv[t_kmt:t0])**2).mean()
+        return res,A,y_ref,ye
+    
+    def cal_gwl(at, τ, mask_param):
+        T0 = Klf.T(np.arange(len(Klf.precipitation)),τ) #np.exp(-np.arange(len(Klf.precipitation))/τ)
+        β = signal.convolve(Klf.precipitation, T0)[Klf.num0:len(Klf.precipitation)]
+        ave = at[:,1].copy() #/at[:,0]*1E2
+        ave[np.isnan(ave)] = 0
+        β = β-β.mean()
+        number = min(len(ave),len(β))
+        u0 = signal.detrend(ave[0:number]) 
+        u1 = signal.detrend(β[0:number]) 
+        u0[np.logical_not(mask_param[0:number])]=0
+        u1[np.logical_not(mask_param[0:number])]=0
+        ccc = u0 @ u1
+
+        γ = ccc / (u1@u1)
+        res = -2*ccc*γ + γ*γ*u1@u1 + u0@u0
+        #β = (gwl[num:number+num]).copy()
+    
+        β = β-β.mean()
+        #print(len(ccf_gwl[number-1-int(τ/2):number-1+int(τ/2)]))
+        number = min(len(ave),len(β))
+        ave = ave[0:number]
+        amp = (np.max(β)- np.min(β))/2
+        β = β * γ
+        return β, ave, res, amp, γ
+
+    def est_h0(ccfs1,ccf_r):
+        h0 = 0.
+        for row in range(3):
+            for col in range(3):
+                ccfs = ccfs1[row][col]
+                #Calc. of reference the CCF
+                mccfs = np.ma.masked_array(ccfs,np.isnan(ccfs))
+                ccf_r[row][col] = mccfs.mean(axis=0)
+                scale = ccfs.dot(ccf_r[row][col])/ccf_r[row][col].dot(ccf_r[row][col])
+                mask = (scale>0.5) & (scale <5.)
+                mccfs[mask] = np.diag(1./scale[mask]) @  np.array(mccfs[mask])
+                ccf_r[row][col] = mccfs[mask].mean(axis=0)
+                h0 += np.mean((mccfs[mask]- ccf_r[row][col])**2)
+        h0 /= 9.
+        return(h0)
+    def eKlf(ccfs_all,ccf_r,delta,ts,te,β,h0,Q0,Pt=np.array([[1E-5,0.],[0.,1E-8]]),at0=np.array([1.,0.])): #β is the precipitation effect
+        mask0 = [False]*1024
+        for i in range(ts,te): mask0[i+512] , mask0[512-i] = True, True
+        #h0 = 1.5**2 #0.08
+        #Ht = h0*np.identity((te-ts)*2)
+        lag_time = ((np.arange(1024)-512)*delta)[mask0]
+        
+        lnL = 0
+        #mask_param = [True]*(len(ccfs_all[0][0])+1)
+        att = np.zeros([len(ccfs_all[0][0]),2])
+        αt = np.zeros([len(ccfs_all[0][0]),2])
+        Ptt = np.zeros([len(ccfs_all[0][0]),2,2])
+        Vt = np.zeros([len(ccfs_all[0][0]),2,2])
+        Qt = np.zeros([2,2])
+        mask_param = np.full([len(ccfs_all[0][0]),3,3], False, dtype=np.bool) 
+            
+        itime = 0
+
+        at = at0.copy() #Initial value
+        Qt = np.array([[Q0[0],0],[0,Q0[1]]]) #Pt*ϵ #[[1E-6,0],[0,1E-9]] #Initial value
+        Tayler_Series = np.zeros([3,3,5,(te-ts)*2]) # Up to 5th order
+        ms_r = np.zeros([3,3])
+
+        for cmp1 in range(3):
+            for cmp2 in range(3):
+                Tayler_Series[cmp1][cmp2] = [Klf.deri(ccf_r[cmp1][cmp2],i,delta)[mask0] for i in range(5)]
+                ms_r[cmp1][cmp2] = np.sum((ccf_r[cmp1][cmp2][mask0]**2))
+                #Zt = np.array(ccf_r[cmp1][cmp2].T,Klf.deri(ccf_r[cmp1][cmp2],1,delta)*lag_time]).T])
+        for iday in range(ccfs_all.shape[2]):
+        #for y_tmp in ccfs_all:
+            yt = ccfs_all[:,:,iday,mask0]
+            Z2 = np.zeros([2,2])
+            γ = np.zeros(2)
+            v2 = 0.
+            count = 0
+            for cmp1 in range(3):
+                for cmp2 in range(3):
+                    Σ = np.zeros([3,(te-ts)*2])
+                    scale = yt[cmp1][cmp2].dot(ccf_r[cmp1][cmp2][mask0])/ms_r[cmp1][cmp2]
+                    Z0, Z1  = np.zeros((te-ts)*2), np.zeros((te-ts)*2)
+                    A, α = at[0], at[1]#/at[0]
+                    if scale > 0.5 and scale < 2:
+                        for i in range(3):
+                            Σ[i] = ((α+β[itime])*lag_time)**i
+                        for i in range(3):
+                            Z0 += (Tayler_Series[cmp1][cmp2][i]/math.factorial(i)*Σ[i])
+                        Z1 = A*(Tayler_Series[cmp1][cmp2][1])
+                        for i in range(1,3):
+                            Z1 += A*(Tayler_Series[cmp1][cmp2][i+1]/math.factorial(i)*Σ[i])
+                            Z1 += A*(Tayler_Series[cmp1][cmp2][i]/math.factorial(i-1)*Σ[i-1])
+                        Z1 *= lag_time
+                        Zt = np.array([Z0.T,Z1.T]).T
+                        ###
+                        Z2 += Zt.T @ Zt
+                        vt = yt[cmp1][cmp2] - A*Z0 #Zt @ at[itime][cmp1][cmp2] #Reduction of β
+                        γ += Zt.T @ vt
+                        v2 += vt.T @ vt
+                        mask_param[itime][cmp1][cmp2] = True
+                        count += 1
+                    #else: print(iday,A,α,scale)
+            Ξ = Pt/h0 - (Pt @ Z2 @ np.linalg.inv(Z2/h0 +np.linalg.inv(Pt)) /(h0**2))
+            #Likelihood
+            λ = np.linalg.eig(Z2 @ Pt)[0]
+            lnL1 = np.log(λ[0]+h0)+np.log(λ[1]+h0)+((te-ts)*2*9-2)*np.log(h0)
+            lnL2 = h0*v2-γ.T @ (np.linalg.inv(Z2/h0 +np.linalg.inv(Pt))) @ γ
+            lnL += -(lnL1+lnL2)/2
+            att[itime] = at + Ξ @ γ # at + Kt @ vt
+            Ptt[itime] = Pt - Ξ @ (Z2 @ Pt @ Z2 + h0 * Z2) @ Ξ.T #Pt - Kt@Ft@Kt.T
+            at[:] = att[itime]      #Tt=1
+            Pt[:] = Ptt[itime] + Qt    #Tt and Rt = 1 
+            itime += 1
+        lnL -= (te-ts)*2*count/2.*np.log(2*np.pi)
+        #Kalman Smoother
+        itmax = itime-1
+        αt[itmax] = att[itmax]
+        Vt[itmax] = Ptt[itmax]
+        for itime in reversed(range(itmax)):
+            Pt1 = Ptt[itime]+Qt
+            At = Ptt[itime]*np.linalg.inv(Pt1)
+            αt[itime] = att[itime]+ At @ (αt[itime+1]-att[itime+1])
+            Vt[itime] = Ptt[itime]+ At @ (Vt[itime+1]-Pt1) @ At.T
+                
+        #return att, Ptt, lnL, mask_param
+        return αt, Vt, lnL, mask_param
+
+    def cal_ref(ccfs_all,at,delta):
+        ccf_r2 = np.zeros([ccfs_all.shape[0],ccfs_all.shape[1],ccfs_all.shape[3]])
+        lag_time = (np.arange(1024)-512)*delta
+
+        for itime in range(ccfs_all.shape[2]):
+            α0 = 0
+            α0 = at[itime][1]#/at[itime][0]
+            for cmp1 in range(3):
+                for cmp2 in range(3):
+                    y_tmp = ccfs_all[cmp1][cmp2][itime]
+                    lag_time2 = (np.arange(1024)-512)*delta*(1+α0)
+                    f2 = interpolate.interp1d(lag_time, y_tmp, kind="quadratic", fill_value='extrapolate')
+                    ccf_r2[cmp1][cmp2] += f2(lag_time2)
+        for cmp1 in range(3):
+            for cmp2 in range(3):
+                ccf_r2[cmp1][cmp2]  /= ccfs_all.shape[2]
+        return ccf_r2
+
+    def T(t,τ,flag = "Exp"):
+        if flag == "Talwani": #Talwani
+            Res = (t+1E-4)**-1.5*np.exp(-1.5*τ/(t+1E-4))
+            Res *= np.sqrt(3*τ/2/np.pi)*1E3*9.8*1E-3 #ρ=1E3, g=9.8
+        elif flag == "Exp": #Exponential
+            Res = np.exp(-t/τ)
+        return Res
+    #from numpy import fft
+    def deri(data,n,delta):
+        if n > 0:
+            dω = 1./(len(data)*delta)*np.pi*2.
+            spctrm = np.fft.rfft(data)
+            for i in range(len(spctrm)): spctrm[i] = spctrm[i]*(1j*(i)*dω)**n 
+            return np.fft.irfft(spctrm)
+        elif n == 0: return(data)
+        
+        
+    #########################
+    #GWL 
+    def cal_gwl_LSQ(at,τ,p,thresh,mask_param):
+        with open("./gwl_ebino_seikei.dat","r") as f:
+            lines = f.readlines()
+        gwl = np.array([float(line.split()[4]) for line in lines])
+
+        T0 =Klf.T(np.arange(len(gwl)),τ)
+        T0[0]=0
+
+        for idx in range(len(gwl)):
+            if gwl[idx] > thresh : gwl[idx] = thresh
+        gwl = signal.convolve(gwl**p, T0)[:len(gwl)]
+
+        for i in range(len(lines)):
+            if int(lines[i].split()[5]) == 1: num = i
+                
+        β = (gwl[num:]).copy()
+        ave = at[:,1]*1E2 #/at[:,0]*1E2
+        ave[np.isnan(ave)] = 0
+        β = β-β.mean()
+        number = min(len(ave),len(β))
+        u0 = signal.detrend(ave[0:number]) 
+        u1 = signal.detrend(β[0:number]) 
+        u0[np.logical_not(mask_param[0:number])]=0
+        
+        ccf_gwl = signal.correlate(u0,u1)
+        #idx1 = np.argmin(ccf_gwl)-(number-1)
+        Δτ = int(τ/2)
+        idx0 = np.argmin(ccf_gwl[number-1-Δτ:number-1+Δτ])-Δτ
+        γ = np.min(ccf_gwl[number-1-Δτ:number-1+Δτ])/u1[mask_param[0:number]].dot(u1[mask_param[0:number]])
+        res = -2*np.min(ccf_gwl[number-1-Δτ:number-1+Δτ])*γ + γ*γ*u1[mask_param[0:number]].dot(u1[mask_param[0:number]]) + u0[mask_param[0:number]].dot(u0[mask_param[0:number]])
+        #print("##",idx0)
+        β = (gwl[num-idx0:number+num-idx0]).copy()
+        #print(len(β),num+idx0,number+num+idx0,len(gwl))
+        β = β-β.mean()
+        #print(len(ccf_gwl[number-1-int(τ/2):number-1+int(τ/2)]))
+        number = min(len(ave),len(β))
+        ave = ave[0:number]
+        amp = (np.max(β)- np.min(β))/2
+        β = β * γ
+        return β, ave, res, amp, γ*1E-2,idx0
+
+
+
+
+"""
+   def cal_gwl2(at, Pt,thresh,mask_param): #Moveing averageo of gwl
+        with open("./gwl_ebino_seikei.dat","r") as f:
+            lines = f.readlines()
+        gwl = np.array([float(line.split()[4]) for line in lines])
+
+        b = signal.hanning(7)
+        b = b/b.sum()
+        gwl = signal.convolve(gwl, b)[:len(gwl)]
+
+        for i in range(len(lines)):
+            if int(lines[i].split()[5]) == 1: num = i
+                
+        β = (gwl[num:]).copy()
+        ave = at[:,1]*1E2 #/at[:,0]*1E2
+        ave[np.isnan(ave)] = 0
+        β = β-β.mean()
+        number = min(len(ave),len(β))
+        u0 = signal.detrend(ave[0:number]) 
+        u1 = signal.detrend(β[0:number]) 
+        u0[np.logical_not(mask_param[0:number])]=0
+                    
+        ccf_gwl = u0[mask_param[0:number]].dot(u1[mask_param[0:number]])
+        γ = ccf_gwl/u1[mask_param[0:number]].dot(u1[mask_param[0:number]])
+        res = -2*ccf_gwl*γ + γ*γ*u1[mask_param[0:number]].dot(u1[mask_param[0:number]]) + u0[mask_param[0:number]].dot(u0[mask_param[0:number]])
+        β = (gwl[num:number+num]).copy()
+        β = β-β.mean()
+        number = min(len(ave),len(β))
+        ave = ave[0:number]
+        β = β * γ
+        return β, ave, res
+"""
---
+++ est_param_with_quake.py
@@ -0,0 +1,135 @@
+from scipy import signal
+from scipy import optimize
+from scipy import fftpack
+import time
+import datetime
+import numpy as np
+import h5py
+import pickle
+
+FLAG_PLOT = False
+if FLAG_PLOT: 
+    import matplotlib.pyplot as plt
+
+from KalmanFilter import Klf
+stnm = {"00":"EvKVO","01":"EvSMW","02":"EvSMN","03":"EvTKS","04":"EvTKW","05":"NTKOF","06":"SMJNH","07":"SKRMV","08":"BKRMV","09":"SKRHV","10":"BKRHV"}
+date_kumamoto = datetime.datetime(2016,4,16)
+date0 = datetime.datetime(2010,5,1)
+date1 = datetime.datetime(2018,9,1)
+Δ     = datetime.timedelta(days=1)
+t_kmt = (date_kumamoto-date0).days
+
+counter = 0
+
+#def likelihood(qm, flag=0,ref=0): #q0[0]: amp, q0[1] = σm^2, q0[2] = τ, q0[3] = amp, q0[4] = delay, q0[5] = Pt[0], q0[6] = Pt[1], q0[7] = at0[1]
+def likelihood(qm, flag=0,ref=0): #q0[0]: amp, q0[1] = σm^2, q0[2] = τ, q0[3] = amp, q0[4] = delay, q0[5] = at0[0], q0[6] = at0[1]
+    q0 = qm * Klf.q_ref
+    #T0 = np.exp(-np.arange(len(Klf.precipitation))/np.exp(q0[2]))
+    T0 = Klf.T(np.arange(len(Klf.precipitation)),q0[2])
+    Y = fftpack.fft(Klf.precipitation)
+    Y2= Y*np.exp(2j*np.pi*fftpack.fftfreq(len(Y))*(-q0[4]))
+    y2 = np.real(fftpack.ifft(Y2))
+    β = signal.convolve(y2, T0)[Klf.num0:len(y2)]
+    β = β-β.mean()
+    β = q0[3]*signal.detrend(β)
+    tx = np.arange(0,len(β),1)
+    
+    if FLAG_PLOT:
+        global counter
+        if counter %45 == 0: plt.plot(β)
+    β[t_kmt:] += (q0[6]*np.exp(-(tx[t_kmt:]-t_kmt)/q0[7]))
+    if FLAG_PLOT:
+        if counter %45== 0:plt.plot(β)
+
+    αt,Vt,lnL, mask_param = Klf.eKlf(ccfs1,ccf_r,delta,ts,te,β,h0,[q0[0],q0[1]], Pt=np.array([[q0[0],0],[0,q0[1]]]),at0 = [1,q0[5]])
+    if FLAG_PLOT:
+        if counter %45 == 0:plt.plot(αt[0:len(β),1])
+        if counter %45 == 0:plt.show()
+        counter += 1
+    
+    if flag == 0:
+        np.set_printoptions(precision=2,linewidth=200)
+        print(q0,lnL-ref,lnL)
+        np.set_printoptions(precision=8)
+        return(-lnL+ref)
+    if flag == 1:
+        print(q0,lnL)
+        return(αt,Vt,lnL, β, mask_param )
+
+
+
+freq_band = "0.15to0.90"
+file = "./long-STS-2_bp"+freq_band+"white.h5"
+
+Klf.init()
+αt_all = {}
+Vt_all = {}
+mask_all = {}
+q_all = {}
+β_all = {}
+
+with h5py.File(file,'r') as h5file:
+    delta= (int)(h5file.attrs['delta']*1000+0.5)/1000
+    ts = int(20/delta)
+    te = int(100/delta)
+    start = time.time()
+    mask_tm = [False]*1024
+    for i in range(ts,te): mask_tm[i+512] , mask_tm[512-i] = True, True
+    win =  np.zeros(1024)
+    win2= signal.tukey(te-ts,.1)
+    for i in range(ts,te): win[i+512] , win[512-i] = win2[i-ts], win2[i-ts]
+    ########
+    npts = h5file.attrs['npts']
+    #station0 = ["00","01","02","03","04","06","07","09"]
+    station0 = ["00","01","02","03","04","05","06","07","08","09","10"]
+    sta_pairs = []
+    for i in range(len(station0)):
+        for j in range(i+1,len(station0)):
+            sta_pairs.append((station0[j],station0[i]))
+    
+    for sta_pair in sorted(list(set(sta_pairs))): #[("04","03")]: [("07","04")]:#[("06","03")]: #[("02","01")]: #
+        print("#",sta_pair,"Time:{0}".format(time.time() - start))
+        ccfs1 = h5file[sta_pair[0]][sta_pair[1]]["ccfs"][:,:,:,:]*win
+        ave = np.zeros([ccfs1.shape[0],ccfs1.shape[1],ccfs1.shape[2]])
+        ccf_r = np.zeros([ccfs1.shape[0],ccfs1.shape[1],ccfs1.shape[3]])
+        β  = np.zeros(len(ccfs1[0][0])+1)
+        ave= np.zeros(len(ccfs1[0][0])+1)
+        
+        h0 = Klf.est_h0(ccfs1,ccf_r)
+        print(h0)
+        β.fill(0)
+        αt,Vt,lnL, mask_param = Klf.eKlf(ccfs1,ccf_r,delta,ts,te,β,h0,[1E-6,1E-9])
+        print("Likelihood = ",lnL)
+        #Search for the optimized paramters
+
+        mask2 = [np.count_nonzero(tmp)==9 for tmp in mask_param] #mask_param.copy()
+        mask2[2100:-1] = [False]*(len(mask2)-2100-1) #Cut after Kumamoto earthquake
+
+        ##For precipitation
+        Klf.q_ref[2] = 100. #days[idx]
+        β2,ave,res, amp, γ  = Klf.cal_gwl(αt,Klf.q_ref[2],mask2)
+        β[0:len(β2)] = β2
+        Klf.q_ref[3] = γ
+        print("gamma = ", γ)
+        
+        #For Kumamoto earthauake return res,y_ref,X,ye
+        Klf.q_ref[7] = 30.
+        res,A,y_ref,ye  = Klf.cal_kumamoto(t_kmt, αt,Klf.q_ref[7],mask2)
+        Klf.q_ref[6] = A
+        #Klf.q_ref[9] = y_ref
+
+        q_init = [1.,1.,1.,1.,1.,1.,1.,1.]        
+        bounds = [[5E-2,2E1],[1E-3,2E1],[0.1,2.],[0.1,2.],[0,2],[-10,10],[1E-3,1E1],[0.1,10]]
+        q_est, min, log=optimize.fmin_l_bfgs_b(lambda q_tmp:likelihood([q_tmp[0],q_tmp[1],q_tmp[2],q_tmp[3],q_tmp[4],q_tmp[5],q_tmp[6],q_tmp[7]],ref=lnL)\
+                                               ,q_init,bounds=bounds, approx_grad = True,)#pgtol=1E-6,epsilon=1E-8,iprint=-1
+        print("#",log)
+        αt,Vt,lnL, β, mask_param = likelihood(q_est,flag=1)
+        αt_all[sta_pair] = αt
+        Vt_all[sta_pair] = Vt
+        mask_all[sta_pair] = mask_param
+        q_all[sta_pair] = q_est*Klf.q_ref
+        β_all[sta_pair] = β
+data_all = {"αt":αt_all, "Vt":Vt_all,"β":β_all, "mask":mask_all, "qm":q_all}
+
+with open('out_weq'+str(datetime.date.today())+'.pickle', mode='wb') as fo:
+    pickle.dump(data_all, fo)
