e73d071f35f2621115a1c8b0d52d76fa0a3ad5cd
knishida <knishida@eri.u-tokyo.ac.jp>
2020-07-26 16:55:59 +0900

    Revisions of about comment lines.

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -5,18 +5,25 @@ from scipy import interpolate
 from scipy import fftpack
 import datetime
 
-
+# The class of the extended Kalman filter
 class Klf:  
     def init():#Initialize precipitation data
         with open("./gwl_ebino_seikei.dat","r") as f: lines = f.readlines()
         Klf.precipitation = np.array([float(line.split()[4]) for line in lines])
-        for i in range(len(lines)):
+        for i in range(len(lines)): # The search for the 1st day of seismic data
             if int(lines[i].split()[5]) == 1: 
                 Klf.num0 = i
         Klf.precipitation -= Klf.precipitation.mean()
-        #Klf.q_ref = np.array([1E-6,1E-9,100.,6.726449064255413e-07,-5.,1E-5,1E-8,1E-3])
         Klf.q_ref = np.array([1E-6,1E-9,100.,6.726449064255413e-07,-5.,1E-3,-1E-3,30.])
-    def cal_kumamoto(t_kmt, at, τ, mask_param):
+        
+    # The initial guess for the explanatory variable of the Kumamoto earthquake.
+    # We assumed that an exponential type response.
+    #
+    # t_kmt: Date of the Kumamoto earthquake
+    # at: the intial estimation of seismic velocity changes without the explanatory variables
+    # τ: The decay time of the repsonse
+    # mask_param: Data mask
+    def cal_kumamoto(t_kmt, at, τ, mask_param): 
         dv = at[:,1].copy() 
         t0 = len(dv[:])
         tx = np.arange(0,t0,1)
@@ -38,6 +45,12 @@ class Klf:
         res = ((ye[t_kmt:t0]-dv[t_kmt:t0])**2).mean()
         return res,A,y_ref,ye
     
+    # The initial guess for the explanatory variable of the ground water level.
+    # We assumed that an exponential type response.
+    #
+    # at: the intial estimation of seismic velocity changes without the explanatory variables
+    # τ: The decay time of the repsonse
+    # mask_param: Data mask
     def cal_gwl(at, τ, mask_param):
         T0 = Klf.T(np.arange(len(Klf.precipitation)),τ) #np.exp(-np.arange(len(Klf.precipitation))/τ)
         β = signal.convolve(Klf.precipitation, T0)[Klf.num0:len(Klf.precipitation)]
@@ -63,6 +76,12 @@ class Klf:
         β = β * γ
         return β, ave, res, amp, γ
 
+    # Estimation of a prior data covariance h0.  h_0 id estimated from the time average 
+    # of the squared difference between observed CCFs and the reference (see the text). 
+    # 
+    # [Input] ccfs1: observed CCFs
+    #         ccf_r: the reference CCF 
+    # [Output] a prior data covariance h0
     def est_h0(ccfs1,ccf_r):
         h0 = 0.
         for row in range(3):
@@ -78,7 +97,23 @@ class Klf:
                 h0 += np.mean((mccfs[mask]- ccf_r[row][col])**2)
         h0 /= 9.
         return(h0)
-    def eKlf(ccfs_all,ccf_r,delta,ts,te,β,h0,Q0,Pt=np.array([[1E-5,0.],[0.,1E-8]]),at0=np.array([1.,0.])): #β is the precipitation effect
+   
+    # An implementation of the extended Kalman filter and smoother
+    #
+    # [Input] ccfs_all: observed CCFs
+    #         ccf_r: the reference CCF
+    #         delta: the sampling interval [s]
+    #         ts: the start of lag time [index number]
+    #         te: the end of the lag time [index number]
+    #         β: the temporal changes estimated by the explanatory variables
+    #         h0: a prior data covariance
+    #         Q0: a prior model covariance
+    #         Pt: A priori model covariance for the initial value
+    #         at0: A priori initial amplitude
+    # [Output] αt: Estimated conditional mean values
+    #          Vt: Estimated model covariance 
+    #          mask_param: the mask of estimated values
+    def eKlf(ccfs_all,ccf_r,delta,ts,te,β,h0,Q0,Pt=np.array([[1E-5,0.],[0.,1E-8]]),at0=np.array([1.,0.])):
         mask0 = [False]*1024
         for i in range(ts,te): mask0[i+512] , mask0[512-i] = True, True
         #h0 = 1.5**2 #0.08
@@ -160,9 +195,13 @@ class Klf:
             αt[itime] = att[itime]+ At @ (αt[itime+1]-att[itime+1])
             Vt[itime] = Ptt[itime]+ At @ (Vt[itime+1]-Pt1) @ At.T
                 
-        #return att, Ptt, lnL, mask_param
         return αt, Vt, lnL, mask_param
 
+    # Calclation of the reference CCF
+    # [Input] ccfs_all: Observed CCFs
+    #         at: The initial guess of the stretching factor
+    #         delra: sampling interval [s]
+    # [Output] ccf_r2: The reference CCF
     def cal_ref(ccfs_all,at,delta):
         ccf_r2 = np.zeros([ccfs_all.shape[0],ccfs_all.shape[1],ccfs_all.shape[3]])
         lag_time = (np.arange(1024)-512)*delta
@@ -180,7 +219,12 @@ class Klf:
             for cmp2 in range(3):
                 ccf_r2[cmp1][cmp2]  /= ccfs_all.shape[2]
         return ccf_r2
-
+    
+    # Evaluation of the response function for precipitation data
+    # [Input] t: Days
+    #         τ: Decay time [days]
+    #         flag: The response type
+    # [Output] Res: The response
     def T(t,τ,flag = "Exp"):
         if flag == "Talwani": #Talwani
             Res = (t+1E-4)**-1.5*np.exp(-1.5*τ/(t+1E-4))
@@ -188,7 +232,12 @@ class Klf:
         elif flag == "Exp": #Exponential
             Res = np.exp(-t/τ)
         return Res
-    #from numpy import fft
+
+    # Calculation of derivative of data using FFT
+    # [Input] data: an array
+    #         n: the order of the derivative
+    #         delta: sampling interval
+    # [Output] data: the derivative
     def deri(data,n,delta):
         if n > 0:
             dω = 1./(len(data)*delta)*np.pi*2.
@@ -198,8 +247,17 @@ class Klf:
         elif n == 0: return(data)
         
         
-    #########################
-    #GWL 
+    # Estimation of the precipitation effects by a simple data fitting
+    # [Input] at: Initial guess of seismic velocity changes
+    #         τ: The decay time of the response
+    #         p: the power, which determines the nonlinearity. If p = 1, we neglect the nonlinearity.
+    #         thresh: Threshold of the precipitation
+    # [Output] β: Estimated seismic velcity changes by the precipitation model
+    #          ave: at*1E2 with exclusions of the outliers of at. This is just for the plot.
+    #          res: Residual between the data and the model
+    #          amp = (max(β)- min(β))/2
+    #          γ: Amplitudes of the response
+    #          idx0: Delay time
     def cal_gwl_LSQ(at,τ,p,thresh,mask_param):
         with open("./gwl_ebino_seikei.dat","r") as f:
             lines = f.readlines()
@@ -241,38 +299,3 @@ class Klf:
         β = β * γ
         return β, ave, res, amp, γ*1E-2,idx0
 
-
-
-
-"""
-   def cal_gwl2(at, Pt,thresh,mask_param): #Moveing averageo of gwl
-        with open("./gwl_ebino_seikei.dat","r") as f:
-            lines = f.readlines()
-        gwl = np.array([float(line.split()[4]) for line in lines])
-
-        b = signal.hanning(7)
-        b = b/b.sum()
-        gwl = signal.convolve(gwl, b)[:len(gwl)]
-
-        for i in range(len(lines)):
-            if int(lines[i].split()[5]) == 1: num = i
-                
-        β = (gwl[num:]).copy()
-        ave = at[:,1]*1E2 #/at[:,0]*1E2
-        ave[np.isnan(ave)] = 0
-        β = β-β.mean()
-        number = min(len(ave),len(β))
-        u0 = signal.detrend(ave[0:number]) 
-        u1 = signal.detrend(β[0:number]) 
-        u0[np.logical_not(mask_param[0:number])]=0
-                    
-        ccf_gwl = u0[mask_param[0:number]].dot(u1[mask_param[0:number]])
-        γ = ccf_gwl/u1[mask_param[0:number]].dot(u1[mask_param[0:number]])
-        res = -2*ccf_gwl*γ + γ*γ*u1[mask_param[0:number]].dot(u1[mask_param[0:number]]) + u0[mask_param[0:number]].dot(u0[mask_param[0:number]])
-        β = (gwl[num:number+num]).copy()
-        β = β-β.mean()
-        number = min(len(ave),len(β))
-        ave = ave[0:number]
-        β = β * γ
-        return β, ave, res
-"""
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Kirishima
 # Imprementation of Extended Kalman filter for the streching method
+
+* Kalmanfilter.py: An implementation of the extended Kalman filter
+* gwl_ebino_seikei.dat: The precipitation data (AMeDAS) at Ebino station. 
+* long-STS-2_bp0.10to0.40white.h5: The data set of daily cross-correlation functions for all the pair of stations.
+* est_param_with_quake.py: A sample python code for estimating temporal variations of the seismic velocities. 
+
+## Data format of the precipitation data at Ebino (gwl_ebino_seikei.dat)
+YY/MM/DD, YYYY, MM, DD, Precipitation(mm), Days from 4/30 2010 (1 means the 1st day of seismic data on 5/1 2010)
+
+In situ precipitation observations were obtained from the Automated Meteorological Data Acquisition System (AMeDAS) of the Japan Meteorological Agency (JMA) are available at http://www.data.jma.go.jp/obd/stats/etrn/index.php (in Japanese).
+
+## Data format of cross-correlation functions (CCFs)
+The CCFs are stored in HDF5 format. The header includes the station locations, date, station elevation, the sampling interval, number of points, and the components. 
+
+
