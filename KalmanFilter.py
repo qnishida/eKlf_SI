Bugfix: Calculation of the Likelihood in KalmanFilter.py.

KalmanFilter.py
@@ -14,7 +14,9 @@ class Klf:
             if int(lines[i].split()[5]) == 1: 
                 Klf.num0 = i
         Klf.precipitation -= Klf.precipitation.mean()
-        Klf.q_ref = np.array([1E-6,1E-9,100.,6.726449064255413e-07,-5.,1E-3,-1E-3,30.])
+        #Klf.q_ref = np.array([1E-6,1E-9,100.,6.726449064255413e-07,-5.,1E-3,-1E-3,30.])
+        #Klf.q_ref = np.array([1E-4,4E-8,100.,6.726449064255413e-07,-5.,1E-3,-1E-3,30.])
+        Klf.q_ref = np.array([1E-5,4E-9,100.,6.726449064255413e-07,-5.,1E-3,-1E-3,30.])
         
     # The initial guess for the explanatory variable of the Kumamoto earthquake.
     # We assumed that an exponential type response.
@@ -82,7 +84,9 @@ class Klf:
     # [Input] ccfs1: observed CCFs
     #         ccf_r: the reference CCF 
     # [Output] a prior data covariance h0
-    def est_h0(ccfs1,ccf_r):
+    def est_h0(ccfs1,ccf_r,ts,te):
+        mask0 = [False]*1024
+        for i in range(ts,te): mask0[i+512] , mask0[512-i] = True, True
         h0 = 0.
         for row in range(3):
             for col in range(3):
@@ -90,11 +94,12 @@ class Klf:
                 #Calc. of reference the CCF
                 mccfs = np.ma.masked_array(ccfs,np.isnan(ccfs))
                 ccf_r[row][col] = mccfs.mean(axis=0)
-                scale = ccfs.dot(ccf_r[row][col])/ccf_r[row][col].dot(ccf_r[row][col])
+                scale = ccfs[:,mask0].dot(ccf_r[row][col][mask0])/ccf_r[row][col][mask0].dot(ccf_r[row][col][mask0])
                 mask = (scale>0.5) & (scale <5.)
+                h0 += np.mean(((mccfs[mask])[:,mask0]- ccf_r[row][col][mask0])**2)
                 mccfs[mask] = np.diag(1./scale[mask]) @  np.array(mccfs[mask])
                 ccf_r[row][col] = mccfs[mask].mean(axis=0)
-                h0 += np.mean((mccfs[mask]- ccf_r[row][col])**2)
+                #h0 += np.mean(((mccfs[mask])[:,mask0]- ccf_r[row][col][mask0])**2)
         h0 /= 9.
         return(h0)
    
@@ -178,7 +183,8 @@ class Klf:
             λ = np.linalg.eig(Z2 @ Pt)[0]
             lnL1 = np.log(λ[0]+h0)+np.log(λ[1]+h0)+((te-ts)*2*9-2)*np.log(h0)
             lnL2 = h0*v2-γ.T @ (np.linalg.inv(Z2/h0 +np.linalg.inv(Pt))) @ γ
-            lnL += -(lnL1+lnL2)/2
+            #print("Likelihood ",lnL1,lnL2,lnL2*h0**-2)
+            lnL += -(lnL1+lnL2*h0**-2)/2
             att[itime] = at + Ξ @ γ # at + Kt @ vt
             Ptt[itime] = Pt - Ξ @ (Z2 @ Pt @ Z2 + h0 * Z2) @ Ξ.T #Pt - Kt@Ft@Kt.T
             at[:] = att[itime]      #Tt=1

est_param_with_quake.py
@@ -49,7 +49,7 @@ def likelihood(qm, flag=0,ref=0): #q0[0]: amp, q0[1] = σm^2, q0[2] = τ, q0[3] 
     
     if flag == 0:
         np.set_printoptions(precision=2,linewidth=200)
-        print(q0,lnL-ref,lnL)
+        print(q0,lnL-ref,lnL, flush=True)
         np.set_printoptions(precision=8)
         return(-lnL+ref)
     if flag == 1:
@@ -95,10 +95,12 @@ with h5py.File(file,'r') as h5file:
         β  = np.zeros(len(ccfs1[0][0])+1)
         ave= np.zeros(len(ccfs1[0][0])+1)
         
-        h0 = Klf.est_h0(ccfs1,ccf_r)
+        h0 = Klf.est_h0(ccfs1,ccf_r,ts,te)
         print(h0)
         β.fill(0)
-        αt,Vt,lnL, mask_param = Klf.eKlf(ccfs1,ccf_r,delta,ts,te,β,h0,[1E-6,1E-9])
+        #αt,Vt,lnL, mask_param = Klf.eKlf(ccfs1,ccf_r,delta,ts,te,β,h0,[1E-6,1E-9])
+        #αt,Vt,lnL, mask_param = Klf.eKlf(ccfs1,ccf_r,delta,ts,te,β,h0,[2E-5,4E-8])
+        αt,Vt,lnL, mask_param = Klf.eKlf(ccfs1,ccf_r,delta,ts,te,β,h0,[1E-5,4E-9])
         print("Likelihood = ",lnL)
         #Search for the optimized paramters
 
@@ -118,10 +120,11 @@ with h5py.File(file,'r') as h5file:
         Klf.q_ref[6] = A
         #Klf.q_ref[9] = y_ref
 
+        print("#")
         q_init = [1.,1.,1.,1.,1.,1.,1.,1.]        
         bounds = [[5E-2,2E1],[1E-3,2E1],[0.1,2.],[0.1,2.],[0,2],[-10,10],[1E-3,1E1],[0.1,10]]
         q_est, min, log=optimize.fmin_l_bfgs_b(lambda q_tmp:likelihood([q_tmp[0],q_tmp[1],q_tmp[2],q_tmp[3],q_tmp[4],q_tmp[5],q_tmp[6],q_tmp[7]],ref=lnL)\
-                                               ,q_init,bounds=bounds, approx_grad = True,)#pgtol=1E-6,epsilon=1E-8,iprint=-1
+                                               ,q_init,bounds=bounds, approx_grad = True,epsilon=1E-5,iprint=99,disp=1,factr=100000000)#pgtol=1E-6,epsilon=1E-8,iprint=-1
         print("#",log)
         αt,Vt,lnL, β, mask_param = likelihood(q_est,flag=1)
         αt_all[sta_pair] = αt